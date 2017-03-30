eb4a8aa95e6dafc1207f94056fe24c7e436bca8c
Sammy Mae <sammymae@Sammys-MacBook-Pro.local>
2017-03-30 17:53:15 -0400

    Added login html file and methods to main.py

--- blog/database.py
+++ blog/database.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Text, DateTime
 
+from flask.ext.login import UserMixin
+
 import datetime
 
 from . import app
@@ -14,12 +16,20 @@ session = Session()
 
 
 class Entry(Base):
-    
+
     __tablename__ = "entries"
-    
+
     id = Column(Integer, primary_key=True)
     title = Column(String(1024))
     content = Column(Text)
     datetime = Column(DateTime, default=datetime.datetime.now)
-    
-Base.metadata.create_all(engine)    
+
+class User(Base, UserMixin):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
+    email = Column(String(128), unique=True)
+    password = Column(String(128))    
+
+Base.metadata.create_all(engine)
--- blog/views.py
+++ blog/views.py
@@ -4,29 +4,32 @@ from flask import request, redirect, url_for
 from . import app
 from .database import session, Entry
 
-    
+from flask import flash
+from flask.ext.login import login_user
+from werkzeug.security import check_password_hash
+from .database import User
+
 
 @app.route("/")
 # @app.route("/entries/<int:limit>")
 @app.route("/page/<int:page>")
 def entries(page=1):
-   
+
     count = session.query(Entry).count()
-    
+
     try:
         limit = int(request.args.get('limit'))
-        
+
     except (ValueError, TypeError):
         limit = 10
-        
+
     if limit:
         if limit > count:
-            limit = 10 
+            limit = 10
         PAGINATE_BY= limit
     else:
         PAGINATE_BY= 10
-    # import pdb; pdb.set_trace()
-    
+
     page_index = page - 1
 
     start = page_index * PAGINATE_BY
@@ -46,13 +49,13 @@ def entries(page=1):
         has_prev=has_prev,
         page=page,
         total_pages=total_pages
-    ) 
-    
-    
-@app.route("/entry/add", methods=["GET"])    
+    )
+
+
+@app.route("/entry/add", methods=["GET"])
 def add_entry_get():
     return render_template("add_entry.html")
-    
+
 @app.route("/entry/add", methods=["POST"])
 def add_entry_post():
     entry = Entry(
@@ -62,7 +65,7 @@ def add_entry_post():
     session.add(entry)
     session.commit()
     return redirect(url_for("entries"))
-    
+
 @app.route("/entry/<int:id>")
 def entry_view(id):
     #return "reading entry %d" % id
@@ -76,9 +79,9 @@ def entry_edit_get(id):
     entry = entry.filter(Entry.id==id)[0]
     return render_template("entry_edit.html", entry=entry)
 
-@app.route("/entry/<int:id>/edit", methods=["POST"])   
+@app.route("/entry/<int:id>/edit", methods=["POST"])
 def entry_edit_post(id):
-    
+
     entry = session.query(Entry)
     entry = entry.filter(Entry.id==id)[0]
     entry.title = request.form['title']
@@ -86,14 +89,14 @@ def entry_edit_post(id):
     session.add(entry)
     session.commit()
     return render_template("entry_view.html", entry=entry)
-    
+
 @app.route("/entry/<int:id>/delete", methods=["GET"])
 def delete_entry_get(id):
     entry = session.query(Entry)
     entry = entry.filter(Entry.id==id)[0]
     return render_template("delete_entry.html", entry=entry)
 
- 
+
 @app.route("/entry/<int:id>/delete_action")
 def delete_entry_post(id):
 
@@ -103,4 +106,19 @@ def delete_entry_post(id):
     session.delete(entry)
     session.commit()
     return redirect(url_for("entries"))
-    
+
+@app.route("/login", methods=["GET"])
+def login_get():
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    email = request.form["email"]
+    password = request.form["password"]
+    user = session.query(User).filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password):
+        flash("Incorrect username or password", "danger")
+        return redirect(url_for("login_get"))
+
+    login_user(user)
+    return redirect(request.args.get('next') or url_for("entries"))   
